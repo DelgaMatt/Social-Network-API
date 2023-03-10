f6cc89acc2034b76daf88152f45c8c4026d3891e
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-03-09 17:28:50 -0800

    added to userControllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,18 @@ const { User } = require('../models');
 module.exports = {
     getAllUsers(req, res) {
         User.find({})
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err));
     },
 
     getSingleUser(req, res) {
         User.findOne({_id: req.params._Id})
+        .populate({path:'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
         .then((user) =>
             !user
                 ? res.status(404).json({message: 'No user found with that id!'})
